e27d4313c7b105f205e5c44d91f508750a94946b
Itacolomi1 <italo.mtr@gmail.com>
2020-03-25 16:46:49 -0300

    Tela de exibir o Quiz

---
+++ ProjetoJava/Recicla.Presentation/src/recicla/presentation/TelaExibeQuizController.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package recicla.presentation;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author italo
+ */
+public class TelaExibeQuizController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void btnAdicionaPergunta(ActionEvent event) {
+    }
+    
+}
