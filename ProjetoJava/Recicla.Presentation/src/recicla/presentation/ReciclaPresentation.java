c9a2b33677736321809247846ce26bac1e38a616
Itacolomi1 <italo.mtr@gmail.com>
2020-03-27 09:54:34 -0300

    tela de jogo do quiz

--- ProjetoJava/Recicla.Presentation/src/recicla/presentation/ReciclaPresentation.java
+++ ProjetoJava/Recicla.Presentation/src/recicla/presentation/ReciclaPresentation.java
@@ -19,7 +19,7 @@ public class ReciclaPresentation extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("TelaRodada.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("TelaJogoQuiz.fxml"));
         
         Scene scene = new Scene(root);
         
---
+++ ProjetoJava/Recicla.Presentation/src/recicla/presentation/TelaJogosController.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package recicla.presentation;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author italo
+ */
+public class TelaJogosController implements Initializable {
+
+    @FXML
+    private Label txtDinheiro;
+    @FXML
+    private Label txtTempo;
+    @FXML
+    private Label txtTempo1;
+    @FXML
+    private Label txtPergunta;
+    @FXML
+    private Label txtAlternativa1;
+    @FXML
+    private Label txtAlternativa2;
+    @FXML
+    private Label txtAlternativa3;
+    @FXML
+    private Label txtPontuacao;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
