166b42ad83fda0bf88472378a8cdc9a05bf791f8
Itacolomi1 <italo.mtr@gmail.com>
2020-05-18 18:43:32 -0300

    Criação da tela e controller do Jogo da Memória

---
+++ ProjetoJava/Recicla.Presentation/src/recicla/presentation/TelaJogoMemoriaController.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package recicla.presentation;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+/**
+ * FXML Controller class
+ *
+ * @author italo
+ */
+public class TelaJogoMemoriaController implements Initializable {
+
+    @FXML
+    private Label txtDinheiro;
+    @FXML
+    private Label txtTempo;
+    @FXML
+    private Label txtTempo1;
+    @FXML
+    private ImageView img_card_1;
+    @FXML
+    private ImageView img_card_2;
+    @FXML
+    private ImageView img_card_6;
+    @FXML
+    private ImageView img_card_7;
+    @FXML
+    private ImageView img_card_8;
+    @FXML
+    private ImageView img_card_9;
+    @FXML
+    private ImageView img_card_10;
+    @FXML
+    private ImageView img_card_3;
+    @FXML
+    private ImageView img_card_4;
+    @FXML
+    private ImageView img_card_5;
+    @FXML
+    private Label txtPontuacao;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
