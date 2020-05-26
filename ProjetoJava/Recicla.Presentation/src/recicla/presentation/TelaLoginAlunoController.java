74d619965f11ab3f3cf1ebed281ed92a26951fce
Vitor Lupinetti <vitorlupinetti@MacBook-Air-de-Vitor.local>
2020-05-25 21:21:20 -0300

    metodo na api de itens iniciado

--- ProjetoJava/Recicla.Presentation/src/recicla/presentation/TelaLoginAlunoController.java
+++ ProjetoJava/Recicla.Presentation/src/recicla/presentation/TelaLoginAlunoController.java
@@ -106,32 +106,31 @@ public class TelaLoginAlunoController implements Initializable {
     }
     
     private String sendGet(String url) throws Exception {
+ 	
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        // optional default is GET
+        con.setRequestMethod("GET");
+
+        //add request header
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+        }
+        in.close();
+
+        return response.toString();
  
-		
-		URL obj = new URL(url);
-		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
- 
-		// optional default is GET
-		con.setRequestMethod("GET");
- 
-		//add request header
-		con.setRequestProperty("User-Agent", USER_AGENT);
- 
-		int responseCode = con.getResponseCode();
-		System.out.println("\nSending 'GET' request to URL : " + url);
-		System.out.println("Response Code : " + responseCode);
- 
-		BufferedReader in = new BufferedReader(
-		        new InputStreamReader(con.getInputStream()));
-		String inputLine;
-		StringBuffer response = new StringBuffer();
- 
-		while ((inputLine = in.readLine()) != null) {
-			response.append(inputLine);
-		}
-		in.close();
- 
-		return response.toString();
- 
-	}
+    }
 }
