8a990c2d15592951eb403b32e18b0fad3c7b807a
Nathan Reis <nathansilvareis@gmail.com>
2020-06-21 23:44:43 -0300

    Fluxo de manipulação da rodada

---
+++ ProjetoJava/Recicla.Business/src/recicla/business/crud/Rodada.java
@@ -0,0 +1,48 @@
+package recicla.business.crud;
+
+import recicla.comuns.vos.Jogo;
+import recicla.dao.sala.RodadaMySQLDAO;
+
+public class Rodada {
+    private RodadaMySQLDAO rodadaDAO = null;
+    
+    public Rodada() {
+        this.rodadaDAO = new RodadaMySQLDAO();
+    }
+    
+    public boolean validarCadastrarJogo(int rodadaId, Jogo jogo) throws Exception {
+        boolean isValid = false;
+        
+        isValid = true;
+        
+        return isValid;
+    }
+    
+    public boolean validarDeletar(int rodadaId) throws Exception {
+        boolean isValid = false;
+        
+        isValid = true;
+        
+        if(isValid) {
+            // Qual DAO??
+        }
+        
+        return isValid;
+    }
+    
+    public boolean validarDeletarJogo(int rodadaId, int jogoId) throws Exception {
+        boolean isValid = false;
+        
+        isValid = true;
+        
+        return isValid;
+    }
+    
+    public boolean validarInserir(Rodada rodada) throws Exception {
+        boolean isValid = false;
+        
+        isValid = true;
+        
+        return isValid;
+    }
+}
--- ProjetoJava/Recicla.Presentation/src/recicla/presentation/TelaRodadaController.java
+++ ProjetoJava/Recicla.Presentation/src/recicla/presentation/TelaRodadaController.java
@@ -1,26 +1,42 @@
 package recicla.presentation;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import recicla.comuns.helperController.HelperController;
 
 /**
  * FXML Controller class
  */
 public class TelaRodadaController implements Initializable {
+    private ArrayList<String> jogosCadastrados = new ArrayList<String>();
+
     @FXML
-    private Label txt_titulo_rodada;
+    private Button btnAdicionarApagueALuz;
     @FXML
-    private Label txt_titulo_sala2;
+    private Button btnAdicionarJogoMemoria;
     @FXML
-    private Label txt_titulo_sala1;
+    private Button btnEscolherPergunta;
     @FXML
-    private Label txt_titulo_sala11;
+    private Button btnExcluirRodada;
     @FXML
-    private Label txt_titulo_sala111;
+    private Button btnStart;
+    @FXML
+    private Label lblJogosCadastrados;
+    @FXML
+    private Label lblQtdJogosCadasreados;
+    @FXML
+    private Label lblTituloRodada;
+    @FXML
+    private Pane pnlJogosCadastrados;
 
     /**
      * Initializes the controller class.
@@ -32,5 +48,76 @@ public class TelaRodadaController implements Initializable {
 
     @FXML
     private void btnExcluiJogo(ActionEvent event) {
+        System.out.println("Removido: " + this.jogosCadastrados.get(this.jogosCadastrados.size() - 1));
+        this.jogosCadastrados.remove(this.jogosCadastrados.size() - 1);
+        atualizarQtdExibida();
+    }
+
+    @FXML
+    private void btnEscolherPregunta_Click(ActionEvent event) {
+        this.adicionarJogo("Pergunta Quiz");
+    }
+
+    @FXML
+    private void btnAdicionarJogoMemoria_Click(ActionEvent event) {
+        this.adicionarJogo("Jogo da memória");
+    }
+
+    @FXML
+    private void btnAdicionarApagueALuz_Click(ActionEvent event) {
+        this.adicionarJogo("Apague a luz");
+    }
+    
+    private void adicionarJogo(String jogo) {
+        System.out.println("Adicionado: " + jogo);
+        this.jogosCadastrados.add(jogo);
+        atualizarQtdExibida();
+    }
+    
+    private void atualizarQtdExibida() {
+        this.lblQtdJogosCadasreados.setText(Integer.toString(this.jogosCadastrados.size()));
+    }
+
+    @FXML
+    private void btnExcluirRodada_Click(ActionEvent event) {
+        try {
+            System.out.println("Rodada excluída");
+            Parent root = FXMLLoader.load(getClass().getResource("TelaExibeSala.fxml"));
+
+            HelperController.exibirTela(root);
+        } catch(Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    @FXML
+    private void btnStart_Click(ActionEvent event) {
+        try {
+            String proximaTela = "";
+            System.out.println("Rodada iniciada");
+            
+            switch(this.jogosCadastrados.get(0).toLowerCase()) {
+                case "pergunta quiz":
+                    proximaTela = "TelaJogoQuiz";
+                    break;
+                    
+                case "apague a luz":
+                    proximaTela = "TelaAcerteAlvo";
+                    break;
+                    
+                case "jogo da memória":
+                    proximaTela = "TelaJogoMemoria";
+                    break;
+                    
+                default:
+                    throw new Exception("Nenhum jogo cadastrado");
+            }
+            
+            Parent root = FXMLLoader.load(getClass().getResource(proximaTela + ".fxml"));
+
+            HelperController.exibirTela(root);
+        } catch(Exception exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 }
